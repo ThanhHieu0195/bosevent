8fc6733b9f7d5f2e6eec1bd46167ba38c847022e
hieutct <hieutct@solazu.com>
2018-08-03 00:57:58 +0700

    add title blog

--- backend/includes/templates/home/4-section-blogs.php
+++ backend/includes/templates/home/4-section-blogs.php
@@ -1,51 +1,44 @@
+<?php
+$posts = get_posts([
+    'meta_query' => array(
+        array(
+            'key' => 'blog_feature',
+            'value' => '1',
+            'compare' => 'LIKE'
+        )
+    )
+]);
+?>
 <div class="nsection nblog" id="bos-blog">
     <div class="nheading__wrap txt--c">
         <div class="ncontainer">
-            <h5 class="nheading__wrap-h5 heading-h5 f--400 txt--up cl--black">Read from our blog</h5>
-            <h2 class="heading-h2 f--600 txt--cap lh--12 cl--black inlineb-t">Latest from blog
+            <h5 class="nheading__wrap-h5 heading-h5 f--400 txt--up cl--black"><?= translate_i18n('Read from our blog') ?></h5>
+            <h2 class="heading-h2 f--600 txt--cap lh--12 cl--black inlineb-t"><?= translate_i18n('Latest from blog') ?>
                 <div class="nline-title "></div>
             </h2>
         </div>
     </div>
     <ul class="nlist-blog">
-        <li class="item">
-            <div class="nlist-blog__img">
-                <img src="<?php echo $path_template_url ?>/assets/images/banner/banner-04.jpg" alt="" class="nimg nratio--img">
-            </div>
-            <div class="nlist-blog__info">
-                <h3 class="nlist-blog__info-title heading-h3 f--600 lh--11">
-                    <a href="" class="cl--white">Apartamento at ten: a decade of celebrating the everyday home</a>
-                </h3>
-                <p class="nlist-blog__info-des font--14 cl--white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde quibusdam quo non eius maiores tempora modi ipsa laboriosam
-                </p>
-            </div>
-        </li>
-        <li class="item">
-            <div class="nlist-blog__img">
-                <img src="<?php echo $path_template_url ?>/assets/images/banner/banner-02.jpg" alt="" class="nimg nratio--img">
-            </div>
-            <div class="nlist-blog__info">
-                <h3 class="nlist-blog__info-title heading-h3  f--600 lh--11">
-                    <a href="" class="cl--white">Apartamento at ten: a decade of celebrating the everyday home</a>
-                </h3>
-                <p class="nlist-blog__info-des font--14 cl--white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde quibusdam quo non eius maiores tempora modi ipsa laboriosam
-                </p>
-            </div>
-        </li>
-        <li class="item">
-            <div class="nlist-blog__img">
-                <img src="<?php echo $path_template_url ?>/assets/images/banner/banner-03.jpg" alt="" class="nimg nratio--img">
-            </div>
-            <div class="nlist-blog__info">
-                <h3 class="nlist-blog__info-title heading-h3 cl--white f--600 lh--11">
-                    <a href="" class="cl--white">Apartamento at ten: a decade of celebrating the everyday home</a>
-                </h3>
-                <p class="nlist-blog__info-des font--14 cl--white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde quibusdam quo non eius maiores tempora modi ipsa laboriosam
-                </p>
-            </div>
-        </li>
+        <?php
+        foreach ($posts as $post):
+            $background = get_field('blog_background', $post->ID, '');
+            $background_url = wp_get_attachment_image_url($background, 'full');
+            ?>
+            <li class="item">
+                <div class="nlist-blog__img">
+                    <img src="<?= esc_url($background_url) ?>" alt="" class="nimg nratio--img">
+                </div>
+                <div class="nlist-blog__info">
+                    <h3 class="nlist-blog__info-title heading-h3 f--600 lh--11">
+                        <a href="<?= \includes\Bootstrap::bootstrap()->helper->getLinkBlogDetail($post->ID) ?>" class="cl--white"><?= $post->post_title ?></a>
+                    </h3>
+                    <p class="nlist-blog__info-des font--14 cl--white"><?= $post->post_excerpt ?>
+                    </p>
+                </div>
+            </li>
+        <?php endforeach;?>
     </ul>
     <div class="ncontainer txt--c">
-        <a href="" class="nbutton color--1 nsize--b f--500 txt--up">More post</a>
+        <a href="" class="nbutton color--1 nsize--b f--500 txt--up"><?= translate_i18n('more post') ?></a>
     </div>
 </div>
